a14deca0730ef8fa4590602745bcc5177657731b
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-08-01 12:33:31 +0300

    Added bootstrap

--- commerce/src/App.js
+++ commerce/src/App.js
@@ -7,9 +7,11 @@ function App() {
   let cardStyle={
     width:"18rem"
   }
+  let cardStyle={
+    width:"18rem"
+  }
   return (
     <>
-    <Header />
       <div className="App">
         <div className="card" style={cardStyle}>
         <img class="card-img-top" src="https://contents.mediadecathlon.com/p2155551/k$11e0e6f8f6f4906c580007288ce1bdd0/run-active-men-s-running-shoe-blackslashyellow.jpg?format=auto&quality=40&f=800x800" alt="Card image cap"></img>
@@ -18,7 +20,6 @@ function App() {
           </div>
         </div>
       </div>
-    <Footer />  
     </>
 
 
