5746d1b6cff7652a8535e72cf71b8a4152031e34
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-08-01 12:33:31 +0300

    Added bootstrap

--- commerce/src/App.js
+++ commerce/src/App.js
@@ -16,6 +16,7 @@ function App() {
   let cardStyle={
     width:"18rem"
   }
+<<<<<<< HEAD
 
   return (
     <>
@@ -25,6 +26,21 @@ function App() {
     <DisplayProductList cartItems={cartItems} setCartItems={setCartItems}/>
 
     <Footer />  
+=======
+  let cardStyle={
+    width:"18rem"
+  }
+  return (
+    <>
+      <div className="App">
+        <div className="card" style={cardStyle}>
+        <img class="card-img-top" src="https://contents.mediadecathlon.com/p2155551/k$11e0e6f8f6f4906c580007288ce1bdd0/run-active-men-s-running-shoe-blackslashyellow.jpg?format=auto&quality=40&f=800x800" alt="Card image cap"></img>
+          <div className='card-header'>
+            <p className='card-text'>Product Name</p>
+          </div>
+        </div>
+      </div>
+>>>>>>> a14deca (Added bootstrap)
     </>
 
 
