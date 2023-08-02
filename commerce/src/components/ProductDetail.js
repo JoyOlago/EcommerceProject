23caa85aa62f6f967b15cd2671408dc192eeefb2
Joy Olago <joydanielle62@gmail.com>
2023-08-02 22:29:12 +0300

    Feat: Added SearchBar and product detail card

--- commerce/src/components/ProductDetail.js
+++ commerce/src/components/ProductDetail.js
@@ -1,1 +1,15 @@
 import React from "react";
+
+
+const ProductDetails = ({ product }) => {
+  return (
+    <div>
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      {/* Add more details here based on your product data */}
+    </div>
+  );
+};
+
+export default ProductDetails
