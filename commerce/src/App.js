ce38787dc8e6816f06239454c05d8148947b12fa
Joy Olago <joydanielle62@gmail.com>
2023-08-02 16:17:12 +0300

    Feat:used displayProduct component to fetch data to display list of items and appended to App.js

--- commerce/src/App.js
+++ commerce/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'
+import DisplayProductList from './components/ProductList';
 
 function App() {
   let cardStyle={
@@ -10,14 +11,15 @@ function App() {
   return (
     <>
     <Header />
-      <div className="App">
+      {/* <div className="App">
         <div className="card" style={cardStyle}>
         <img class="card-img-top" src="https://contents.mediadecathlon.com/p2155551/k$11e0e6f8f6f4906c580007288ce1bdd0/run-active-men-s-running-shoe-blackslashyellow.jpg?format=auto&quality=40&f=800x800" alt="Card image cap"></img>
           <div className='card-header'>
-            <p className='card-text'>Product Name</p>
-          </div>
+            <p className='card-text'>Product Name</p> */}
+          {/* </div>
         </div>
-      </div>
+      </div> */}
+    <DisplayProductList/>
     <Footer />  
     </>
 
