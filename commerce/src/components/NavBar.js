39b3a30bd0cf8b72a101a246d23cc5d72ae494f3
barakatimothy <timothybarak39@gmail.com>
2023-08-03 15:18:41 +0300

    Feat:worked on the checkout process

--- commerce/src/components/NavBar.js
+++ commerce/src/components/NavBar.js
@@ -1,68 +1,73 @@
 import React, { useState } from "react";
 import "./NavBar.css";
-import  RegistrationForm  from "./Register";
+import RegistrationForm from "./Register";
 
-function NavBar(){
+function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [firstName, setFirstName] = useState('');
 
-    const [isRegistered, setIsRegistered] = useState(false);
+  const handleRegistrationComplete = (firstName) => {
+    setIsLoggedIn(true);
+    setFirstName(firstName);
+  };
 
-    const handleRegistrationComplete = () => {
-        setIsRegistered(true);
-      };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setFirstName('');
+  };
 
-    const navBarStyle={
-        height:"120px",
-        backgroundColor: '#D7BFD4'
-    }
+  const navBarStyle = {
+    height: "120px",
+    backgroundColor: '#D7BFD4'
+  }
 
-    const menuContainerStyle={
-        position: "relative",
-        //marginLeft: "100",
-        float: "left",
-        top: "30%",
-        //border: "3px solid black",
-        width:"80px"
-    }
+  const menuContainerStyle = {
+    position: "relative",
+    float: "left",
+    top: "30%",
+    width: "80px"
+  }
 
-    const menuIconStyle={
-        width:"58px",
-        height: "5px",
-        backgroundColor: "white",
-        margin: "6px 0"
-    }
+  const menuIconStyle = {
+    width: "58px",
+    height: "5px",
+    backgroundColor: "white",
+    margin: "6px 0"
+  }
 
-    const loginRegisterStyle={
-        position:"relative",
-        float: "right",
-        //border: "3px solid black",
-        width: "180px",
-        color: "white",
-        marginRight: "5%",
-        top: "30%"
-    }
+  const loginRegisterStyle = {
+    position: "relative",
+    float: "right",
+    width: "180px",
+    color: "white",
+    marginRight: "5%",
+    top: "30%"
+  }
 
-    return(
-        <>
-         <nav className="navbar-custom" style={navBarStyle}>
-            <div className="container" id="menuContainer" style={menuContainerStyle}>
-             <div style={menuIconStyle}></div>
-                <div style={menuIconStyle}></div>
-                <div style={menuIconStyle}></div>
+  return (
+    <>
+      <nav className="navbar-custom" style={navBarStyle}>
+        <div className="container" id="menuContainer" style={menuContainerStyle}>
+          <div style={menuIconStyle}></div>
+          <div style={menuIconStyle}></div>
+          <div style={menuIconStyle}></div>
+        </div>
+        <div class="row" style={loginRegisterStyle}>
+          {isLoggedIn ? (
+            <div className="col">
+              <h6>Hello, {firstName}!</h6>
+              <button onClick={handleLogout}>Logout</button>
             </div>
-            <div class="row" style={loginRegisterStyle}>
-                <div className="col" style={{borderRight: "2px solid white"}}>
-                    <h6>Login</h6>
-                </div>
-                <div className="col">
-                   {/* Show different content based on registration status  */}
-                  {isRegistered ? (
-                 <h6>Registered Successfully!</h6>) : (
-                  <RegistrationForm onRegistrationComplete={handleRegistrationComplete} />
-                   )}
-                </div>
+          ) : (
+            <div className="col">
+              <h6>Login</h6>
+              <RegistrationForm onRegistrationComplete={handleRegistrationComplete} />
             </div>
-        </nav>   
-        </>
-    )
+          )}
+        </div>
+      </nav>
+    </>
+  )
 }
-export default NavBar
+
+export default NavBar;
