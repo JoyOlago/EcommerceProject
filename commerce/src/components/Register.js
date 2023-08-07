e503fa5c67bab00826ee08b18e87ee4f28baa074
barakatimothy <timothybaraka39@gmail.com>
2023-08-07 03:21:51 -0400

    Feat:Added functionality register

--- commerce/src/components/Register.js
+++ commerce/src/components/Register.js
@@ -1,178 +1,153 @@
 import React, { useState } from 'react';
-import closeIcon from '../Assets/Navbar/close.svg'
+import closeIcon from '../Assets/Navbar/close.svg';
 
-const RegistrationForm = ({ navBarStyle, menuContainerStyle, loginRegisterStyle }) => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-  });
+const RegistrationForm = () => {
+const [first_name, setfName] = useState('');
+const [last_name, setlName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [showSignupForm, setShowSignupForm] = useState(true);
+const [submitted, setSubmitted] = useState(false);
+const [error, setError] = useState(false);
 
-  const [showSignupForm, setShowSignupForm] = useState(true);
+const handlefName = (e) => {
+setfName(e.target.value);
+setSubmitted(false);
+};
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
+const handlelName = (e) => {
+setlName(e.target.value);
+setSubmitted(false);
+};
 
-  const handleRegisterClick = () => {
-    setShowSignupForm(!showSignupForm);
-  };
+const handleEmail = (e) => {
+setEmail(e.target.value);
+setSubmitted(false);
+};
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
+const handlePassword = (e) => {
+setPassword(e.target.value);
+setSubmitted(false);
+};
 
-    try {
-      const response = await fetch('http://ecommerce.muersolutions.com/api/v1/user/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+const handleRegisterClick = () => {
+setShowSignupForm(!showSignupForm);
+};
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Registration successful!', data);
-        // You can handle successful registration here, e.g., redirect to a new page, show a success message, etc.
-      } else {
-        console.error('Registration failed!');
-        // You can handle registration failure here, e.g., show an error message.
-      }
-    } catch (error) {
-      console.error('Error during registration!', error);
-    }
-  };
+const handleSubmit = (e) => {
+e.preventDefault();
+if (first_name === '' || last_name === '' || email === '' || password === '') {
+setError(true);
+setSubmitted(false);
+} else {
+setSubmitted(true);
+setError(false);
+// You can perform form submission actions here.
+}
+};
 
-  const backgroundHider={
-    position: "fixed",
-    width:"100%",
-    height:"100%",
-    backgroundColor: "rgba(50,50,50,0.9)",
-    zIndex: "9"
-  }
+const successMessage = () => {
+return (
+<div className="success" style={{ display: submitted ? '' : 'none' }}>
+<h1>User {first_name} successfully registered!!</h1>
+</div>
+);
+};
 
-  const registrationFormStyle = {
-    position: "fixed",
-    top: "30%",
-    left: "30%",
-    //display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    width: "500px",
-    height: "300px",
-    backgroundColor: " #F5C096",
-    //backgroundColor: "rgba(0,0,0,0.2)",
-    borderRadius: "30px",
-    fontFamily: "Franklin-Gothic-Medium",
-    zIndex: "9"
-  }
+const errorMessage = () => {
+return (
+<div className="error" style={{ display: error ? '' : 'none' }}>
+<h1>Please enter all the fields</h1>
+</div>
+);
+};
 
-  const inputStyle={
-    width:"100px"
-  }
+const backgroundHider = {
+position: 'fixed',
+width: '100%',
+height: '100%',
+backgroundColor: 'rgba(50,50,50,0.9)',
+zIndex: '9'
+};
+
+const registrationFormStyle = {
+position: 'fixed',
+top: '30%',
+left: '30%',
+justifyContent: 'center',
+alignItems: 'center',
+textAlign: 'center',
+width: '500px',
+height: '300px',
+backgroundColor: ' #F5C096',
+borderRadius: '30px',
+fontFamily: 'Franklin-Gothic-Medium',
+zIndex: '9'
+};
+
+const inputStyle = {
+width: '100px'
+};
 
   return (
     <>
       {showSignupForm && (
-        <div className='backgroundHider' style={backgroundHider}>
-                    <div className='popup' style={registrationFormStyle}>
-            <div className='container' style={{padding:"10px"}}>
-              <div className='row'>
-                <div className='col'>
-                  <h3 style={{fontSize:"25px", float: "left"}}>Register with Us</h3>
-                </div>
-                <div className='col'>
-                  <a onClick={handleRegisterClick} style={{ position: "relative", float: "right" }}>
-                  <img style={{ width: "50px", height: "50px" }} src={closeIcon}></img>
+        <div className="backgroundHider" style={backgroundHider}>
+          <div className="popup" style={registrationFormStyle}>
+            <div className="container" style={{ padding: '10px' }}>
+              <div className="row">
+                <h3 style={{ fontSize: '25px', float: 'left' }}>Register With Us</h3>
+                <div className="col">
+                  <a onClick={handleRegisterClick} style={{ position: 'relative', float: 'right' }}>
+                    <img style={{ width: '50px', height: '50px' }} src={closeIcon} alt="Close" />
                   </a>
                 </div>
               </div>
             </div>
-            <div className='container'>
-              <form onSubmit={handleFormSubmit}>
-                <div className='container' style={{}}>
-                  <div className='row'>
-                    <div className='col'>
-                      <p htmlFor="firstName">First Name:</p>
+            <div className="container">
+              <form onSubmit={handleSubmit}>
+                <div className="container">
+                  <div className="row">
+                    <div className="col">
+                      <label>First Name:</label>
+                      <input name="first_name" id="first_name" value={first_name} onChange={handlefName} />
                     </div>
-                    <div className='col'>
-                      <input
-                      type="text"
-                      id="firstName"
-                      name="firstName"
-                      value={formData.firstName}
-                      onChange={handleInputChange}
-                      />
+                    <div className="col">
+                      <label>Last Name:</label>
+                      <input name="last_name" id="last_name" value={last_name} onChange={handlelName} />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <label>Email:</label>
+                      <input name="email" id="email" value={email} onChange={handleEmail} />
+                    </div>
+                    <div className="col">
+                      <label>Password:</label>
+                      <input name="password" id="password" value={password} onChange={handlePassword} />
                     </div>
                   </div>
                 </div>
-                <div className='container' style={{}}>
-                  <div className='row'>
-                    <div className='col'>
-                      <p htmlFor="lastName">Last Name:</p>
-                    </div>
-                    <div className='col'>
-                      <input
-                      type="text"
-                      id="lastName"
-                      name="lastName"
-                      value={formData.lastName}
-                      onChange={handleInputChange}
-                      />
-                    </div>
-                  </div>
+                <div className="container">
+                  <button className="btn btn-primary" type="submit">
+                    Register
+                  </button>
                 </div>
-                <div className='container' style={{}}>
-                  <div className='row'>
-                    <div className='col'>
-                      <p htmlFor="email">Email:</p>
-                    </div>
-                    <div className='col'>
-                      <input
-                      type="text"
-                      id="email"
-                      name="email"
-                      value={formData.email}
-                      onChange={handleInputChange}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className='container' style={{}}>
-                  <div className='row'>
-                    <div className='col'>
-                      <p htmlFor="password">Password:</p>
-                    </div>
-                    <div className='col'>
-                      <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      value={formData.password}
-                      onChange={handleInputChange}
-                  />
-                    </div>
-                  </div>
-                </div>
-                <div className='container'>
-                  <button className='btn btn-primary' type="submit">Register</button>
-                </div>
-                
               </form>
             </div>
           </div>
         </div>
-
       )}
+
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+      </div>
     </>
   );
-}
+};
 
 export default RegistrationForm;
-
 
 
 
